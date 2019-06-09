6198111885f2c73a88957dbf2e4afa528012c945
peakhunt <hawk.kim1974@gmail.com>
2019-06-09 18:39:58 +0900

    working on slave registers

--- src/components/ModbusSlave.vue
+++ src/components/ModbusSlave.vue
@@ -9,8 +9,74 @@
          />
       </v-flex>
     </v-layout>
+
     <v-layout row wrap>
+      <v-flex xs12>
+        <v-toolbar flat>
+          <v-toolbar-title>Registers</v-toolbar-title>
+          <v-spacer></v-spacer>
+
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ on }">
+              <v-btn color="primary" dark class="mb-2" v-on="on">Add New</v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">Add New Registers</span>
+              </v-card-title>
+              <v-card-text>
+                <v-layout wrap>
+                  <v-flex xs12>
+                    <v-select
+                     v-model="addNewDlg.type"
+                     :items="registerTypes"
+                     label="Register Type"
+                    />
+                  </v-flex>
+                  <v-flex xs12>
+                    <v-text-field
+                     label="Address"
+                     v-model="addNewDlg.address"
+                    />
+                  </v-flex>
+                  <v-flex xs12>
+                    <v-text-field
+                     label="Description"
+                     v-model="addNewDlg.desc"
+                    />
+                  </v-flex>
+                  <v-flex xs12>
+                    <v-text-field
+                     label="Value"
+                     v-model="addNewDlg.value"
+                    />
+                  </v-flex>
+                </v-layout>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" flat @click="dialog = false">Cancel</v-btn>
+                <v-btn color="blue darken-1" flat @click="onAddRegisters">Save</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+        <v-data-table
+         :headers="headers"
+         :items="slave.registerList"
+         class="elevation-1"
+        >
+          <template v-slot:items="props">
+            <td>{{ props.item.type }}</td>
+            <td>{{ props.item.address }}</td>
+            <td>{{ props.item.desc }}</td>
+            <td>{{ props.item.value }}</td>
+          </template>
+        </v-data-table>
+      </v-flex>
     </v-layout>
+
   </v-card-text>
 </v-card>
 </template>
@@ -34,6 +100,36 @@ export default {
         this.$store.commit('SLAVE_UPDATE_ADDRESS', { address, slave: this.slave });
       },
     },
+  },
+  methods: {
+    onAddRegisters() {
+      this.$store.dispatch('slaveAddRegister', { slave: this.slave, regCfg: this.addNewDlg });
+      this.dialog = false;
+    },
+  },
+  data() {
+    return {
+      dialog: false,
+      headers: [
+        { text: 'Type', value: 'type' },
+        { text: 'Address', value: 'address' },
+        { text: 'Description', value: 'desc' },
+        { text: 'Value', value: 'value', sortable: false },
+      ],
+      items: [],
+      registerTypes: [
+        'coil',
+        'discrete',
+        'holding',
+        'input',
+      ],
+      addNewDlg: {
+        type: '',
+        address: 1,
+        desc: '',
+        value: 0,
+      },
+    };
   },
 };
 </script>
--- src/store/modules/slave.js
+++ src/store/modules/slave.js
@@ -1,9 +1,20 @@
+import Vue from 'vue';
+
 const mutations = {
   SLAVE_ADD_NEW(s, commPort) {
     const slave = {
       address: 0,
       registers: {
+        coil: {
+        },
+        discrete: {
+        },
+        holding: {
+        },
+        input: {
+        },
       },
+      registerList: [],
     };
 
     commPort.slaves.push(slave);
@@ -19,6 +30,20 @@ const mutations = {
 
     slave.address = address;
   },
+  SLAVE_ADD_REG(s, payload) {
+    const { slave, regCfg } = payload;
+    const reg = JSON.parse(JSON.stringify(regCfg));
+
+    slave.registerList.push(reg);
+    Vue.set(slave.registers[reg.type], reg.address, reg);
+  },
+  SLAVE_DEL_REG(s, payload) {
+    const { slave, reg } = payload;
+    const ndx = slave.registerList.indexOf(reg);
+
+    slave.registerList.splice(ndx, 1);
+    Vue.delete(slave.registers[reg.type], reg.address);
+  },
 };
 
 const actions = {
@@ -28,9 +53,19 @@ const actions = {
   slaveDel(context, payload) {
     context.commit('SLAVE_DEL', payload);
   },
+  slaveAddRegister(context, payload) {
+    context.commit('SLAVE_ADD_REG', payload);
+  },
+  slaveDelRegister(context, payload) {
+    context.commit('SLAVE_DEL_REG', payload);
+  },
+};
+
+const getters = {
 };
 
 export default {
   mutations,
   actions,
+  getters,
 };
